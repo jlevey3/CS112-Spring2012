c5faf9d76aad885fe208a63d6b90fc974fe6bf59
Josh Levey <jll09@hampshire.edu>
2012-02-10 01:34:15 -0500

    hw04 done

--- hw04/board.py
+++ hw04/board.py
@@ -8,11 +8,10 @@ width=int(raw_input("Please enter your checkerboard width: "))
 heights=0
 widths=0
 
-while widths!=width:
-	print (b+w)*(width/2)
-	print (b+w)*(height)
-	widths+=1
-	heights+=1
-#if heights<height:
-#	print (b+w)*(height/2)
-#	heights+=1
+while heights<height:
+		print (b+w)*(width/2)
+		print (b+w)*(height)
+		widths+=1
+		heights+=1
+		if widths==width:
+			break
--- hw04/cakeordeath.py
+++ hw04/cakeordeath.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 #Filename: cakeordeath.py
-# THIS IS INCOMPLETE
 slices = 3
 running = True
 while running:
 	response = raw_input("Cake or death? ")
-	if response == "Cake" or "cake":
+	if response == "Cake":
 		print "Here is a slice of cake!"
 		slices-=1
-	if slices==0:
+		if slices==0:
 			print "I am oh so sorry, you have eaten all of the cake."
-	elif response == "death" or "Death":
+	elif response == "cake":
+			print "Here is a slice of cake!"
+			slices-=1
+			if slices==0:
+				print "I am oh so sorry, you have eaten all of the cake."
+	elif response == "Death":
 		print "You will have all of your skin sliced open so that your insides fall out before being beheaded."
 		running=False 
+	elif response == "death":
+		print "You will be hung and shot at."
+		running=False
 print "Loop over!!"
 		
